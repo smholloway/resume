871b32ac073806b45ddf2452b95aeb81dbb78e0e
Seth Holloway <smholloway@gmail.com>
2014-10-29 17:06:03 -0500

    Improve the build script

--- latex/build.sh
+++ latex/build.sh
@@ -2,9 +2,11 @@
 
 if [ -z "$1" ]
   then
-    echo "No argument supplied"
-    echo "./build.sh resume"
+    echo "No argument supplied. Please supply the TeX filename as a parameter."
+    echo "Example: ./build.sh SethHollowayResume"
     exit 0
 fi
 
-pdflatex $1 && open $1.pdf
+pdflatex $1
+rm $1.log $1.out $1.aux
+open $1.pdf
